02ceb54e7a4ba0002c8a8433995d388a0ee8cecd
Alex Schmaus <94810049+AlexSchmaus@users.noreply.github.com>
2023-01-12 14:24:51 -0500

    isRegexMatch Function

---
+++ isRegexMatch.cs
@@ -0,0 +1,45 @@
+//
+// Alex Schmaus
+// 1/12/2023
+//
+// Checks a string against a Regular Expression, to see if there is a match
+//
+
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+using System.Text.RegularExpressions;
+
+
+// Reference Material:
+// SQL data types to CLR types: https://learn.microsoft.com/en-us/sql/relational-databases/clr-integration-database-objects-types-net-framework/mapping-clr-parameter-data?view=sql-server-ver16
+// C# Regex: https://learn.microsoft.com/en-us/dotnet/api/system.text.regularexpressions.regex?view=net-7.0
+
+
+// note to self - partial classes can be defined across mult. files
+public partial class UserDefinedFunctions
+{
+    // dictate function's behavior in for SQL
+    [Microsoft.SqlServer.Server.SqlFunction(Name = "CLR_isRegexMatch")]
+    // Function with two params, str, and pattern. 
+    public static SqlBoolean isRegexMatch(SqlString str, SqlString pattern)
+    {
+        // declare the return
+        SqlBoolean r;
+
+        // Create regx object, with the provided pattern. convert the SqlString to a string
+        Regex rx = new Regex((string)pattern);
+        
+        // check to see if its a match!
+        if (rx.IsMatch((string)str)) {
+            r = true;
+        }
+        else {
+            r = false;
+        }
+        
+        return r;
+    }
+}
